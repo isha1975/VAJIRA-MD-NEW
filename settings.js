3ac3158d83e55f10ab4c587bdfc4db343f0af9fc
isha1975 <143393309+isha1975@users.noreply.github.com>
2024-09-10 18:58:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,12 +5,12 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=DQU1HbhR#vs4jA6MH4J6tZBTj79-5Lef91fz1B12q8ZeXK90SKIs' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=nUtmGKpR#ucTjSREmdGwWLiIk6p9CFqQI8OQi1Gc3JKJgG8Lu_kc' : process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94707330800' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
-ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
+ONLY_ME: process.env.ONLY_ME === undefined ? 'true' : process.env.ONLY_ME,
 AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || false  ,    
-PREFIX: process.env.PREFIX || ',' ,
+PREFIX: process.env.PREFIX || '.' ,
 FOOTER: process.env.FOOTER=== undefined ? '‌👨‍💻 ᴠᴀᴊɪʀᴀ ᴍᴅ ʙʏ ᴛᴄ ᴛᴇᴀᴍ 👨‍💻': process.env.FOOTER,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,
 MAX_SIZE: 500, 
